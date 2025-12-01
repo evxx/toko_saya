2b4d7cf2b70b4f4ac2a16af2bd70fbc39565b8cb
evxx <evania0908@gmail.com>
2025-12-01 15:41:05 +0700

    change database credentials to env

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',       
-    host: 'localhost',
-    database: 'toko_online',     
-    password: '',   
-    port: 5432,                 
+    user: process.env.DB_USER || 'postgres',       
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'toko_online',     
+    password: process.env.DB_PASS || '',   
+    port: process.env.DB_PORT || 5432,                 
 });
 
 // Tes koneksi
