995e575f20a0e6531add91954c02377b499c5a8d
evxx <evania0908@gmail.com>
2025-12-01 16:23:25 +0700

    modify database connection string new

--- db/pool.js
+++ db/pool.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     // database: process.env.DB_NAME || 'toko_online',     
     // password: process.env.DB_PASS || '',   
     // port: process.env.DB_PORT || 5432,       
-    connectionString: process.env.DB_CONN          
+    connectionString: process.env.DB_CONN || 'postgresql://postgres.nbtbhczulihwyhatiycy:$4sBJgC3Z%?5hrf@aws-1-ap-south-1.pooler.supabase.com:5432/postgres'
 });
 
 // Tes koneksi
