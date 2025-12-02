d60d2e95666284f9f32e1af8e7ca29f05173a78c
evxx <evania0908@gmail.com>
2025-12-02 08:54:06 +0700

    Add new server

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
+const fs = require('fs')
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',       
+    user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'toko_online',     
-    password: process.env.DB_PASS || '',   
+    database: process.env.DB_NAME || 'toko_online',
+    password: process.env.DB_PASS || '',
     port: process.env.DB_PORT || 5432,
     //connectionString: process.env.DB_CONN || 'postgresql://postgres.nbtbhczulihwyhatiycy:$4sBJgC3Z%?5hrf@aws-1-ap-south-1.pooler.supabase.com:5432/postgres'
 });
@@ -12,6 +13,14 @@ const pool = new Pool({
 // Tes koneksi
 pool.connect((err, client, release) => {
     if (err) {
+        const filePath = "../logs/db.log"
+        fs.promises.appendFile(filePath, err, (fsError) => {
+
+            if (fsError) {
+                console.error('Error appending to file:', fsError);
+                return;
+            }
+        })
         return console.error('Error saat koneksi ke database', err.stack);
     }
     console.log('Koneksi ke database PostgreSQL berhasil!');
